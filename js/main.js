dabf5fbf56332440fd107a62355f79df323a97dc
Jonathan See <mannysee@gmail.com>
2017-11-07 17:35:39 +1100

    finish

--- js/main.js
+++ js/main.js
@@ -1,6 +1,25 @@
-var xhr = new XMLHttpRequest();
-xhr.open("GET", "https://api.equitise.exchange/offer", false);
-xhr.send();
-// Add your code below!
-xhr.status
-console.log(xhr.status)
+function numberWithCommas(x) {
+  return x.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+$(document).ready(function() {
+  
+  $.ajax({
+    url: "https://api.equitise.exchange/offer"
+  })
+  .done(function(res){
+    console.log(res);
+    for (var i = 0; i < res.comingSoon.length; i++) {
+      var item = res.comingSoon[i];
+      
+      // create box and boxInner
+      var $box = $('<div>').addClass('box').appendTo('#box-container');
+      var $boxInner = $('<div>').addClass('boxInner').appendTo($box);
+      
+      // put the name and raisedAmount in boxInner
+      $('<h3>').text(item.name).appendTo($boxInner);
+      $('<h4>').text('Raised Amount: $' + numberWithCommas(item.raisedAmount)).appendTo($boxInner);
+      $('<h4>').text('Private: ' + item.private).appendTo($boxInner);
+    }
+  })
+});
